9b87155d3eddf810fa7564d3cd7624354435081c
Virdes <virdes_barbosa@hotmail.com>
2023-08-17 00:49:43 +0300

    version 1.5

---
+++ src/app/portfolio/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './page.module.css'
+
+const Layout = ({children}) => {
+  return (
+    <div>
+        <h1 className={styles.mainTitle}>My works</h1>
+        {children}
+    </div>
+  )
+}
+
+export default Layout
